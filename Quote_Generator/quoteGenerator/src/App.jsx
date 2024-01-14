7858a3a88f6938c121213826818bbc325ad05793
Aphomer <aaphomer@gmail.com>
2024-01-14 04:35:32 +0100

    Initializing Random Quote Generator

---
+++ Quote_Generator/quoteGenerator/src/App.jsx
@@ -0,0 +1,14 @@
+// import QuoteFetcher from 'Quote.jsx';
+import QuoteFetcher from "./Quote"
+
+function App() {
+
+  return (
+    <>
+      <QuoteFetcher />
+      
+    </>
+  )
+}
+
+export default App
---
+++ Quote_Generator/quoteGenerator/src/Quote.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+const RandomQuoteURL = "https://inspo-quotes-api.herokuapp.com/quotes/random";
+
+function QuoteFetcher() {
+    const [quote, setQuote] = useState({ text: "", author: "" });
+
+    useEffect(() => {
+       fetchQuote();
+    }, []);
+
+    // useEffect(() => {
+    //     async function getInitialQuote() {
+    //         const res = await fetch(RandomQuoteURL);
+    //         const jsonRes = await res.json();
+    //         const randomQuote = jsonRes.quote;
+    //         setQuote(randomQuote);
+    // }
+    // getInitialQuote();
+    // }, []);
+
+
+    async function fetchQuote() {
+        const res = await fetch(RandomQuoteURL);
+        const jsonRes = await res.json();
+        const randomQuote = jsonRes.quote;
+        setQuote(randomQuote);
+
+    } 
+
+    return (
+        <div>
+            <button onClick={fetchQuote}>Get Quote Using Handler</button>
+            <h1>{quote.text}</h1>
+            <h3>{quote.author}</h3>
+        </div>
+    )
+}
+
+export default QuoteFetcher;
+
