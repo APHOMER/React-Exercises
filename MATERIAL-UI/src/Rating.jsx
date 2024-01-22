079a74a4f34c287a9c6fcbc310a68b82a4cbd4e3
Aphomer <aaphomer@gmail.com>
2024-01-22 12:09:51 +0100

    Attaching Rating Component

---
+++ MATERIAL-UI/src/Rating.jsx
@@ -0,0 +1,21 @@
+import Rating from '@mui/material/Rating';
+import { useState } from 'react';
+
+export default function RatingComponent(){
+    const [score, setScore] = useState(3);
+
+    return (
+        <div>
+            <h1>{score <= 0 ? "There is no Score" : `The current Score is Rated ${score}`}</h1>
+            {/* <h1>The current Score is Rated {score}</h1> */}
+            <Rating
+                name="simple-controlled"
+                value={score}
+                onChange={(event, newValue) => {
+                    setScore(newValue);
+                }}
+            />
+        </div>
+        
+    )
+}
